2d3923966e91861fd852539383a2f9b235fc9996
unknown <wujz@cn.ibm.com>
2020-06-16 18:58:21 -0700

    upgrade to python 3

--- src/STATS_MCSET_CONVERT.py
+++ src/STATS_MCSET_CONVERT.py
@@ -3,7 +3,7 @@
 # *
 # * IBM SPSS Products: Statistics Common
 # *
-# * (C) Copyright IBM Corp. 1989, 2014
+# * (C) Copyright IBM Corp. 1989, 2020
 # *
 # * US Government Users Restricted Rights - Use, duplication or disclosure
 # * restricted by GSA ADP Schedule Contract with IBM Corp. 
@@ -94,7 +94,7 @@ def catvalues(mcset, varprefix, setname):
     table.SimplePivotTable(rowdim=_("Variable Name"),
         rowlabels=generatedvars,
         collabels=[_("Value Represented"), _("Label")],
-        cells = zip(generatedvalues, generatedlabels))
+        cells = list(zip(generatedvalues, generatedlabels)))
     spss.EndProcedure()
     
 def genSetsCategoryList(mcset, allvars, resolver, setname, varprefix):
@@ -227,7 +227,7 @@ def attributesFromDict(d):
     """build self attributes from a dictionary d."""
     
     self = d.pop('self')
-    for name, value in d.iteritems():
+    for name, value in d.items():
         setattr(self, name, value)
 
 class Resolver(object):
@@ -243,7 +243,7 @@ class Resolver(object):
         self.varlist = self.ds.varlist
         self.mrsets = {}
         # the api always returns the set name in upper case
-        for name, theset in self.ds.multiResponseSet.data.iteritems():
+        for name, theset in self.ds.multiResponseSet.data.items():
             self.mrsets[name.upper()] = theset
             
     def close(self):
@@ -292,7 +292,7 @@ class Resolver(object):
 def Run(args):
     """Execute the STATS CATEGORY ORDER command"""
 
-    args = args[args.keys()[0]]
+    args = args[list(args.keys())[0]]
     ###print args   #debug
     
 
@@ -311,7 +311,7 @@ def Run(args):
             return msg
 
         # A HELP subcommand overrides all else
-    if args.has_key("HELP"):
+    if "HELP" in args:
         #print helptext
         helper()
     else:
@@ -331,7 +331,7 @@ def helper():
     # webbrowser.open seems not to work well
     browser = webbrowser.get()
     if not browser.open_new(helpspec):
-        print("Help file not found:" + helpspec)
+        print(("Help file not found:" + helpspec))
 try:    #override
     from extension import helper
 except:
